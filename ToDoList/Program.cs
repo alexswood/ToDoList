9ef099e4fa740863245a08013e2d59666b7bcb34
Alex <alex35wood@gmail.com>
2020-02-08 15:33:05 +0000

    create-todo-item: create a ToDoItem object from the users input

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -21,14 +21,20 @@ namespace ToDoList
                 case Option.None:
                     break;
                 case Option.Create:
-                    Console.WriteLine("\r\nAwesome, you want to create a new ToDo, let's get started...");
-                    Console.ReadLine();
+                    CreateToDo();
                     break;
                 default:
                     break;
             }
+
+            Console.WriteLine("Press any key to exit");
+            Console.Read();
         }
 
+        /// <summary>
+        /// Present the option(s) and return the users chosen option.
+        /// </summary>
+        /// <returns>An <see cref="Option"/> value indicaing what option the user has chosen.</returns>
         private static Option GetOption()
         {
             Option option = Option.None;
@@ -43,6 +49,24 @@ namespace ToDoList
             }
 
             return option;
+        }
+
+        /// <summary>
+        /// Create a new ToDo item.
+        /// </summary>
+        private static void CreateToDo()
+        {
+            Console.WriteLine("\r\nAwesome, you want to create a new ToDo, let's get started...");
+
+            Console.WriteLine("Title:");
+            string title = Console.ReadLine();
+
+            Console.WriteLine("Description:");
+            string description = Console.ReadLine();
+
+            ToDoItem item = new ToDoItem(title, description);
+
+            Console.WriteLine($"\r\nGreat! You've just created a new ToDo with title: '{item.Title}' and descripton: '{item.Description}'.");
         }
     }
 }
